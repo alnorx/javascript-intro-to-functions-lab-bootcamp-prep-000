7501c325512953d79839cbcdc024b4e2753bf40d
Alan N <anan112114@gmail.com>
2018-11-29 14:54:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {return `${string}`.toUpperCase()}
+
+function whisper(string) {return `${string}`.toLowerCase()}
+
+function logShout(string) {console.log(`${string}`.toUpperCase(`${string}`))}
+
+function logWhisper(string) {console.log(`${string}`.toLowerCase(`${string}`))}
+
+function sayHiToGrandma(string) { var lowercase = `${string}`; 
+if (lowercase.toLowerCase() === lowercase)
+{return "I can\'t hear you!"}
+
+else if(lowercase.to
+
+
+
